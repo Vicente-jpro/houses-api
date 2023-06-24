8bdb7c3a7abec89a7788911c15574fcb79ec2ae2
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-06-24 14:11:56 +0100

    Seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cadastrando as provincias..."
+Province.find_or_create_by!(province_name: "Luanda")
+Province.find_or_create_by!(province_name: "Úige")
+Province.find_or_create_by!(province_name: "Uila")
+Province.find_or_create_by!(province_name: "Kuanza Sul")
+puts "Fim do cadastro de provincias."
+
+
+puts "Cadastrando municipios/cidade..."
+City.find_or_create_by!(city_name: "Belas", province: Province.first)
+City.find_or_create_by!(city_name: "Rangel", province: Province.first)
+City.find_or_create_by!(city_name: "Cazenga", province: Province.first)
+City.find_or_create_by!(city_name: "Viana", province: Province.first)
+puts "Fim do cadastro das municipios/cidade."
+
+
+puts "Cadastrando as cidades...."
+Address.find_or_create_by!(street: "Centralidade do Kilamba", city: City.all.sample)
+Address.find_or_create_by!(street: "Rangel rua 17", city: City.all.sample)
+Address.find_or_create_by!(street: "Talalice", city: City.all.sample)
+Address.find_or_create_by!(street: "Mutamba", city: City.all.sample)
+puts "Fim do cadastro das cidades."
+
+puts "Cadastrando as provincias..."
+Property.find_or_create_by!(property_description: "Luanda")
+Property.find_or_create_by!(property_description: "Úige")
+Property.find_or_create_by!(property_description: "Uila")
+Property.find_or_create_by!(property_description: "Kuanza Sul")
+puts "Fim do cadastro de provincias."
