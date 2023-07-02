2226f959bba447ad9074ec5d49f1d7a41926b252
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-07-02 16:11:49 +0100

    Next attribute added

--- app/models/land.rb
+++ app/models/land.rb
@@ -1,7 +1,5 @@
 class Land < ApplicationRecord
   belongs_to :dimension
-  accepts_nested_attributes_for :dimension, allow_destroy: true
-
-  belongs_to :address
-  accepts_nested_attributes_for :address, allow_destroy: true
+  belongs_to :Address
+  accepts_nested_attributes_for :address, :dimension, allow_destroy: true
 end
